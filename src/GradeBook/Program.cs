b97b74b730fb05a1f68e984505dacf699319c3ac
David Gress <dgress@lgamerica.com>
2020-01-31 16:27:06 -0500

    first commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            var book = new Book("Dave's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"Average: {stats.Average:N1}");
+            Console.WriteLine($"Lowest Grade: {stats.Low:N1}");
+            Console.WriteLine($"Highest Grade: {stats.High:N1}");
+        }
+    }
+}
---
+++ test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+// if you are in the namespace you don't need using
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        [Fact]  // a piece of fact, xunit looks for these, a decorator
+        public void Test1()
+        {
+            // 3 sections
+            // arrange section
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);          
+
+            // act section
+            var result = book.GetStatistics();
+
+            // assert section, triple "A"
+            Assert.Equal(85.6, result.Average, 1);
+            Assert.Equal(90.5, result.High, 1);
+            Assert.Equal(77.3, result.Low, 1);
+        }
+    }
+}
